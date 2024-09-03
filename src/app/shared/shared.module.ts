fed9f85e525a61bf174391a139bbadded243bce1
jcst2019 <juansolar_4@hotmail.com>
2024-09-02 22:20:06 -0500

    agregando ng g c shared/pages/contact  --skip-tests

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,20 +3,27 @@ import { CommonModule } from '@angular/common';
 
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContactComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
