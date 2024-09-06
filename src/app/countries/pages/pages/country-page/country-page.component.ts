82cde18e9ce26b4e9ceea036e9bc929cf4748cb6
jcst2019 <juansolar_4@hotmail.com>
2024-09-06 18:55:12 -0500

    1 Observable dentro de otro Observable

--- src/app/countries/pages/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/pages/country-page/country-page.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
   styles: ``
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+
+  constructor(
+    private  activatedRoute: ActivatedRoute,
+    private countriesService:CountriesService
+  ){
+
+  }
+  ngOnInit(): void {
+    //Primera Version
+    // this.activatedRoute.params.subscribe(
+    //   // (params:any)=>{ //No se recomeinda usar el tipo de dato ANY
+    //   //   console.log(params);
+    //   //   console.log(params.id);
+        
+    //   // }
+    //   params=>{
+    //     console.log(params);
+    //     console.log(params['id']);
+        
+    //   }
+    // );
+
+    // Segunda Version
+    // this.activatedRoute.params
+    // .subscribe(({id})=>{
+    //     console.log({params:id});
+        
+    //   }
+    // );
+
+    // Tercera Versión
+    this.activatedRoute.params
+    .subscribe(({id})=>{
+        this.countriesService.searchCountryByAlphaCode(id)
+        .subscribe(
+          country => {
+            console.log(country);
+            
+          }
+        );
+        
+      }
+    );
+  }
 
 }
