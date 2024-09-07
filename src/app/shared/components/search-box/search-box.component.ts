b5f10b33c7374561eae43389fa530dbd84717bad
jcst2019 <juansolar_4@hotmail.com>
2024-09-07 08:05:28 -0500

    creando debouncer

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,21 +1,39 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
 
+  // private debouncer:Subject<string> = new Subject<string>();
+  private debouncer = new Subject<string>();
+  
   @Input()
   public placeHolder:string='';
-
+  
   @Output()
   // public onValue: EventEmitter<string> =new EventEmitter<string>();
   public onValueJCST = new EventEmitter<string>();
+  
+  ngOnInit(): void {
+    this.debouncer
+    .subscribe(value=>{
+      console.log('debouncer value',value);
+      
+    });
+  }
 
   public emitValue(value:string):void{
     this.onValueJCST.emit(value);
+  }
+
+  onKeyPress(searchTerm:string){
+
+    // console.log(searchTerm);
+    this.debouncer.next(searchTerm);
   }
 
 }
