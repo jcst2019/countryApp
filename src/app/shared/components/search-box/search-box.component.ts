b3b91ccdfd33a2e3c1249b36e2701fe3c7555bf3
jcst2019 <juansolar_4@hotmail.com>
2024-09-07 08:40:33 -0500

    limpieza de suscripciones

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,15 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent implements OnInit {
-
+export class SearchBoxComponent implements OnInit, OnDestroy {
+  
   // private debouncer:Subject<string> = new Subject<string>();
   private debouncer = new Subject<string>();
+
+  private debouncerSuscription?: Subscription;
   
   @Input()
   public placeHolder:string='';
@@ -17,13 +19,13 @@ export class SearchBoxComponent implements OnInit {
   @Output()
   // public onValue: EventEmitter<string> =new EventEmitter<string>();
   public onValueJCST = new EventEmitter<string>();
-
+  
   @Output()
-
+  
   public onDebounce = new EventEmitter<string>();
   
   ngOnInit(): void {
-    this.debouncer
+    this.debouncerSuscription=this.debouncer
     .pipe(
       debounceTime(1000) //1000 =< 1 segundo// Pero se recomienda 300 o 3 miesimas de segundo
     )
@@ -31,6 +33,13 @@ export class SearchBoxComponent implements OnInit {
       console.log('debouncer value',value);
       this.onDebounce.emit(value);
     });
+  }
+  
+  ngOnDestroy(): void {
+    // throw new Error('destruido');
+    console.log('destruido');
+    this.debouncerSuscription?.unsubscribe();
+    
   }
 
   public emitValue(value:string):void{
